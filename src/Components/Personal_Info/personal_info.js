35268de849adf70528425af91e7c92079f41df3f
sIR-ThIs-WaY <yamminghin@gamil.com>
2020-04-03 18:44:17 +0800

    change personal_info component to function component

--- src/Components/Personal_Info/personal_info.js
+++ src/Components/Personal_Info/personal_info.js
@@ -1,18 +1,14 @@
 import React from 'react';
 import classes from './personal_info.module.css';
 
-class personal_info  extends React.Component{
-    constructor(props){
-        super(props);
-    }
-
-    render(){
+const personal_info = (props) => {
         return(
         <div className={classes.Personal_info}>
             <p>Name: Alex</p>
             <p>Hobby: Reading, playing video games(including RingFit Advanture)</p>
-        </div>)
-    }
+        </div>
+        )
+    
 }
 
 export default personal_info;
