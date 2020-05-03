a29df88998ce2b03ddedabaede77f2451aea2628
rm-rf-rm-rf-rm-rf <maybe_later@maybe_later.com>
2020-05-03 16:04:15 +0800

    Updates

---
+++ precache-manifest.647d17b33b44f3fa221fd42f8b5cdef3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1452e6c2d85e4f8b176b90b2b57bbe01",
+    "url": "/personal_page/index.html"
+  },
+  {
+    "revision": "8cb8a45ffaaf96fbe23b",
+    "url": "/personal_page/static/css/main.ab710a9b.chunk.css"
+  },
+  {
+    "revision": "ba9bfa777bfa17f9fc34",
+    "url": "/personal_page/static/js/2.c622ebdc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/personal_page/static/js/2.c622ebdc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8cb8a45ffaaf96fbe23b",
+    "url": "/personal_page/static/js/main.bac569b3.chunk.js"
+  },
+  {
+    "revision": "931b80456e1a992b6539",
+    "url": "/personal_page/static/js/runtime-main.5d1b05b6.js"
+  },
+  {
+    "revision": "3b178e39365232d622cd46d0b46200f8",
+    "url": "/personal_page/static/media/winnie.3b178e39.JPG"
+  }
+]);
