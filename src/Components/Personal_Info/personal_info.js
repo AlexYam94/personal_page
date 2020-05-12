5a5ead2d1a6008270c47ceeebf9e7540a63be788
rm-rf-rm-rf-rm-rf <maybe_later@maybe_later.com>
2020-05-12 20:41:51 +0800

    change personal info

--- src/Components/Personal_Info/personal_info.js
+++ src/Components/Personal_Info/personal_info.js
@@ -1,15 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState} from 'react';
 import classes from './Personal_info.module.css';
 
-const personal_info = (props) => {
-        return(
+const Personal_info = (props) => {
+    const [test, setTest] = useState(null);
+
+    useEffect(() => {
+    }, [])
+
+    return(
         <div className={classes.Personal_info}>
-            <p>Name: Alex</p>
+            {/* <p>Name: Alex</p>
             <p>Hobby: Reading, playing video games</p>
             <p>Introvert</p>
+            <p>test get api: {test}</p> */}
+            <p>Hello World</p>
         </div>
         )
     
 }
 
-export default personal_info;
+export default Personal_info;
