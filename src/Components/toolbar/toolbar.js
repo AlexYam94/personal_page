117b0f5781720b741fcf44e8796e4649ef3e2dd4
rm-rf-rm-rf-rm-rf <maybe_later@maybe_later.com>
2021-05-05 21:21:17 +0800

    Add pencil circle

--- src/Components/toolbar/toolbar.js
+++ src/Components/toolbar/toolbar.js
@@ -18,12 +18,34 @@ const toolbar = props => (
             <div className={classes.spacer}></div>
             <div className={classes.toolbar_navigation_items}>
                 <ul>
-                    <li><a href="javascript:;" onClick={() => {props.toolbarClickHandler('main')}}>main</a></li>
+                        <li className={classes.button}><a href="javascript:;"onClick={() => {props.toolbarClickHandler('main')}}>main</a>
+                            <svg viewBox="0 0 500 150" preserveAspectRatio="none">
+                                {pencilCircle()}
+                            </svg>
+                        </li>
                     {/* <li><a href="javascript:;" onClick={() => {props.toolbarClickHandler('game')}}>mini games</a></li> */}
-                    <li><a href="javascript:;" onClick={() => {props.toolbarClickHandler('tic_tac_toe')}}>tic tac toe</a></li>
-                    <li><a href="javascript:;" onClick={() => {props.toolbarClickHandler('hangman')}}>hangman</a></li>
-                    <li><a href="javascript:;" onClick={() => {props.toolbarClickHandler('snake')}}>snake</a></li>
-                    <li><a href="javascript:;" onClick={() => {props.toolbarClickHandler('todo')}}>todo list</a></li>
+                    <li className={classes.button}><a href="javascript:;" onClick={() => {props.toolbarClickHandler('unity_games')}}>unity games</a>
+                            <svg viewBox="0 0 500 150" preserveAspectRatio="none">
+                                {pencilCircle()}
+                            </svg>
+                        </li>
+                    <li className={classes.button}><a href="javascript:;" onClick={() => {props.toolbarClickHandler('tic_tac_toe')}}>tic tac toe</a>
+                            <svg viewBox="0 0 500 150" preserveAspectRatio="none">
+                                {pencilCircle()}
+                            </svg>
+                        </li>
+                    <li className={classes.button}><a href="javascript:;" onClick={() => {props.toolbarClickHandler('hangman')}}>hangman</a>
+                            <svg viewBox="0 0 500 150" preserveAspectRatio="none">
+                                {pencilCircle()}
+                            </svg></li>
+                    <li className={classes.button}><a href="javascript:;" onClick={() => {props.toolbarClickHandler('snake')}}>snake</a>
+                            <svg viewBox="0 0 500 150" preserveAspectRatio="none">
+                                {pencilCircle()}
+                            </svg></li>
+                    <li className={classes.button}><a href="javascript:;" onClick={() => {props.toolbarClickHandler('todo')}}>todo list</a>
+                            <svg viewBox="0 0 500 150" preserveAspectRatio="none">
+                                {pencilCircle()}
+                            </svg></li>
                 </ul>
             </div>
             </Router>
@@ -32,3 +54,7 @@ const toolbar = props => (
 );
 
 export default toolbar;
+
+function pencilCircle() {
+    return <path fill="none" d="M325,18C228.7-8.3,118.5,8.3,78,21C22.4,38.4,4.6,54.6,5.6,77.6c1.4,32.4,52.2,54,142.6,63.7 c66.2,7.1,212.2,7.5,273.5-8.3c64.4-16.6,104.3-57.6,33.8-98.2C386.7-4.9,179.4-1.4,126.3,20.7" />;
+}
